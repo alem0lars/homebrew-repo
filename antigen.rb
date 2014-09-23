cb5251178a43e17c4d67d8476d4197aa172fd502
Alessandro Molari <molari.alessandro@gmail.com>
2014-09-23 22:46:24 +0200

    Antigen updated

--- antigen.rb
+++ antigen.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Antigen < Formula
   homepage 'https://github.com/zsh-users/antigen'
   url      'https://github.com/zsh-users/antigen/archive/v1.tar.gz'
-  sha1     'df5449d100d60b1a72d186ed2718cfe8560d2153'
+  sha1     '8132263205612633791c955a530ea8891d15bfba'
   version  '1'
   head     'https://github.com/zsh-users/antigen.git', using: :git, tag: 'v1'
 
