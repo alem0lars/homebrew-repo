d633b57cb174b92b13b08e7a26ac32177e553c0a
Alessandro Molari <molari.alessandro@gmail.com>
2014-09-23 22:45:24 +0200

    Antigen formula updated

--- antigen.rb
+++ antigen.rb
@@ -1,16 +1,12 @@
 require 'formula'
 
 
-class Boris < Formula
+class Antigen < Formula
   homepage 'https://github.com/zsh-users/antigen'
   url      'https://github.com/zsh-users/antigen/archive/v1.tar.gz'
   sha1     'df5449d100d60b1a72d186ed2718cfe8560d2153'
   version  '1'
-  head     'https://github.com/zsh-users/antigen.git'
-
-  def install
-    libexec.install "v1.tar.gz"
-  end
+  head     'https://github.com/zsh-users/antigen.git', using: :git, tag: 'v1'
 
   test do
     system 'antigen', '-h'
