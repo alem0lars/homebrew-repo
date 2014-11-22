b4863428a5a1f3a05b4f852889172241a0060f19
Alessandro Molari <molari.alessandro@gmail.com>
2014-11-23 00:47:37 +0100

    Updated cargo

--- Formula/cargo.rb
+++ Formula/cargo.rb
@@ -3,8 +3,8 @@ require "formula"
 class Cargo < Formula
   homepage "http://crates.io"
   url "https://github.com/rust-lang/cargo/archive/master.tar.gz"
-  version "wip"
-  head "https://github.com/rust-lang/cargo.git"
+  version "latest"
+  head "https://github.com/rust-lang/cargo.git", using: :git
 
   depends_on "rust"
 
