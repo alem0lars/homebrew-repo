2b5dfaac7084478c4da3640186a96a9f2b7ba01f
Alessandro Molari <molari.alessandro@gmail.com>
2015-10-29 19:18:22 +0100

    Update fizzy.rb

--- Formula/fizzy.rb
+++ Formula/fizzy.rb
@@ -3,6 +3,9 @@ require 'formula'
 
 class Fizzy < Formula
   homepage 'https://github.com/alem0lars/fizzy'
+  url      'https://github.com/alem0lars/fizzy/archive/1.5.0.tar.gz'
+  sha256   'bb0737340c0926c2e1a5947d74e7eebec0f9e21ef4a0f6111a228e973d4149d6'
+  version  '1.5.0'
   head     'https://github.com/alem0lars/fizzy.git', using: :git
 
   test do
